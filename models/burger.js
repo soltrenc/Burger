db796601d3144fdc92299881c9a0f59abf85bf73
Chris Soltren <csoltren1@yahoo.com>
2020-03-24 01:20:18 -0400

    Did the controller and connection

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+const orm = require('../config/orm');
+
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, cb);
+        cb(res);
+    },
+};
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
