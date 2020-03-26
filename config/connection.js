5740c908dd3aad27e76c971ae714cf0904a4da04
Chris Soltren <csoltren1@yahoo.com>
2020-03-26 18:40:47 -0400

    Fixed connection error

--- config/connection.js
+++ config/connection.js
@@ -14,14 +14,6 @@ if (process.env.JAWSDB_URL) {
     });
 }
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
-
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
